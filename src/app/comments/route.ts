a2afcc15825608d8ca53144b950b1f3273c8750a
kyun <fm13071303@rx.tnc.ne.jp>
2024-01-26 13:15:40 +0900

    ✨ Next.js14 -44 完了

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,11 +1,13 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-	return new Response(JSON.stringify(comments), {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+export async function GET(request: NextRequest) {
+	const searchParams = request.nextUrl.searchParams;
+	const query = searchParams.get("query");
+	const filteredComments = query
+		? comments.filter((comment) => comment.text.includes(query))
+		: comments;
+	return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
@@ -16,7 +18,6 @@ export async function POST(request: Request) {
 	};
 	comments.push(newComment);
 	return new Response(JSON.stringify(newComment), {
-		// ここを修正
 		headers: {
 			"Content-Type": "application/json",
 		},
